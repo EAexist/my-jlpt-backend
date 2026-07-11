e46d4828af40d9f05a5b1ec885d6d8612d704646
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-07-11 22:59:01 +0900

    feat(auth): wire BearerAuthGuard into AuthModule providers and exports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,12 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
+import { BearerAuthGuard } from './bearer-auth.guard';
+
 @Module({
   imports: [ConfigModule, PrismaModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, BearerAuthGuard],
+  exports: [AuthService, BearerAuthGuard],
 })
 export class AuthModule {}
