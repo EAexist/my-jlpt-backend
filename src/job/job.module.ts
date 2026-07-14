18e61885d79177d14ed9090738378c684392423d
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-07-14 20:19:18 +0900

    refactor(job): add PrismaModule to JobModule imports

--- src/job/job.module.ts
+++ src/job/job.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { JobService } from './job.service';
 import { JobController } from './job.controller';
 import { JobStatusService } from './job-status/job-status.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [JobController],
   providers: [JobService, JobStatusService],
 })
