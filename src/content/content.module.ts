375f64df91a19be429145d6748edc477d7070899
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-07-14 20:48:53 +0900

    refactor(content): add missing imports to ContentModule

--- src/content/content.module.ts
+++ src/content/content.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+import { StorageModule } from '../storage/storage.module';
+import { NlpModule } from '../nlp/nlp.module';
 import { ContentService } from './content.service';
 import { ContentController } from './content.controller';
 import { ContentIngestionService } from './content-ingestion/content-ingestion.service';
 import { ContentManagementService } from './content-management/content-management.service';
 
 @Module({
+  imports: [PrismaModule, StorageModule, NlpModule],
   controllers: [ContentController],
   providers: [
     ContentService,
