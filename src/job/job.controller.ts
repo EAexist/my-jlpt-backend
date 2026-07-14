9bbc6b413f230764f79820ca34d4d10a5b822662
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-07-14 19:04:41 +0900

    feat(job): implement internal callback endpoint

--- src/job/job.controller.ts
+++ src/job/job.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
 import { JobService } from './job.service';
+import { JobCallbackAuthGuard } from './job-callback-auth.guard';
 
 @Controller('job')
 export class JobController {
   constructor(private readonly jobService: JobService) {}
+
+  @UseGuards(JobCallbackAuthGuard)
+  @Post('/internal/jobs/:jobId/callback')
+  async callback(@Param('jobId') jobId: string, @Body() result: any) {
+    return await this.jobService.handleCallback(jobId, result);
+  }
 }
