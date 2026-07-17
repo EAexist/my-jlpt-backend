53a5955f29f7508b5e6711b4a029ab676df53307
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-07-17 19:22:44 +0900

    refactor(llm): remove llm module to delegate llm tasks to fastapi microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ConfigGlobalModule } from './config/config.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigGlobalModule } from './config/config.module';
+import { ContentModule } from './content/content.module';
+import { GroupModule } from './group/group.module';
 import { HealthModule } from './health/health.module';
 import { JobModule } from './job/job.module';
-import { ContentModule } from './content/content.module';
+import { NlpModule } from './nlp/nlp.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { StorageModule } from './storage/storage.module';
-import { NlpModule } from './nlp/nlp.module';
-import { LlmModule } from './llm/llm.module';
-import { GroupModule } from './group/group.module';
 
 @Module({
   imports: [
@@ -22,7 +21,6 @@ import { GroupModule } from './group/group.module';
     PrismaModule,
     StorageModule,
     NlpModule,
-    LlmModule,
     GroupModule,
   ],
   controllers: [AppController],
