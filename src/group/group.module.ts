4750fbed7dc6f413d3d33d893592fbcab92ce98b
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-07-20 18:35:57 +0900

    feat: import ContentModule in GroupModule

--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GroupService } from './group.service';
 import { GroupController } from './group.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ContentModule } from '../content/content.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ContentModule],
   controllers: [GroupController],
   providers: [GroupService],
 })
